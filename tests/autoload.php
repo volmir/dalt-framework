4dcb0d0eab454d616ab1a02ea702ff8d7420dab9
volmir <volmir@ukr.net>
2017-01-30 18:57:53 +0200

    Add PHPUnit tests

--- tests/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$loader = require(__DIR__ . '/../vendor/autoload.php');
-$loader->addPsr4('Frm\\', __DIR__ . '/../system/');
-$loader->addPsr4('App\\', __DIR__ . '/../application/');
--- tests/unit/ConfigTest.php
+++ tests/unit/ConfigTest.php
@@ -9,9 +9,9 @@ class ConfigTest extends PHPUnit_Framework_TestCase
 
     public function testFilesExists()
     {
-        $this->assertFileExists('../application/configs/db.ini');
-        $this->assertFileExists('../application/configs/routes.ini');
-        $this->assertFileExists('../application/configs/main.ini');
+        $this->assertFileExists('../../application/configs/db.ini');
+        $this->assertFileExists('../../application/configs/routes.ini');
+        $this->assertFileExists('../../application/configs/main.ini');
     }
     
     public function testIsRightType() 
