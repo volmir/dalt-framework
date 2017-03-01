8b5d8a70818eeafbc1a197dbd47305b5a89049fe
Vladimir Prokhnenko <volmir@ukr.net>
2017-03-01 15:57:38 +0200

    Add EventManager

--- system/core/Registry.php
+++ system/core/Registry.php
@@ -5,18 +5,36 @@ namespace framework\core;
 class Registry 
 {
 
-    protected $registry = array();
+    /**
+     *
+     * @var array
+     */
+    protected $registry = [];
 
+    /**
+     * 
+     * @param array $registry
+     */
     public function __construct($registry = []) 
     {
         $this->registry = $registry;
     }
 
+    /**
+     * 
+     * @param string $name
+     * @return mixes
+     */
     public function __get($name) 
     {
         return isset($this->registry[$name]) ? $this->registry[$name] : null;
     }
 
+    /**
+     * 
+     * @param string $name
+     * @param mixed $value
+     */
     public function __set($name, $value) 
     {
         $this->registry[$name] = $value;
